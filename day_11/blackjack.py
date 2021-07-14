fd79dc2080ec047e411d6ff02ee58692213f48e4
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2021-07-14 07:13:48 +0200

    Section 11: Day 11 - Beginner - The Blackjack Capstone Project
    107. Blackjack Program Requirements and Game Rules

--- day_11/blackjack.py
+++ day_11/blackjack.py
@@ -13,9 +13,37 @@
 ############### Our Blackjack House Rules #####################
 #Hint 1: Go to this website and try out the Blackjack game:
 # https://games.washingtonpost.com/games/blackjack/
-
+import random
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
+answer = ""
 computer_cards = []
 human_cards = []
+
+def deal_cards(deck_user,cards_s):
+    deck_user.append(random.choice(cards_s))
+
+def sum_cards(user_deck):
+    sum = 0
+    for card in user_deck:
+        sum += card
+    return sum
+
+def check_for_loser(deck_of_cards):
+    if sum_cards(deck_of_cards) > 21:
+        return False
+    else:
+        return True
+
+deal_cards(human_cards,cards)
+deal_cards(human_cards,cards)
+deal_cards(computer_cards,cards)
+
+print(f"Player cards: {sum_cards(human_cards)} <:-:> Dealer cards: {sum_cards(computer_cards)}")
+
+if check_for_loser(human_cards) and check_for_loser(computer_cards):
+    answer = input("Hit or Stand? (H/S)").strip().lower()
+    if answer == "h":
+        deal_cards(human_cards)
+    elif answer == "s":
+        print("You did great!")
 
