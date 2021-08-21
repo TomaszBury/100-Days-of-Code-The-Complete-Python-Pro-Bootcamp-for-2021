59b83eea7c0f95fe5c4b8bd455312c2d4afae390
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2021-08-21 10:41:49 +0200

    Section 13: Day 13 - Beginner - Debugging: How to Find and Fix Errors in your Code
    122. Reproduce the Bug
    123. Play Computer and Evaluate Each Line

--- day_13/debugging.py
+++ day_13/debugging.py
@@ -1,25 +1,35 @@
 ############DEBUGGING#####################
 
 # Describe Problem
-def my_function():
-  for i in range(1, 21):
-    print(f"{i}",end=" ")
-    if i == 20:
-      print("You got it")
-my_function()
+# def my_function():
+#   for i in range(1, 21):
+#     print(f"{i}",end=" ")
+#     if i == 20:
+#       print("You got it")
+# my_function()
 
 # # Reproduce the Bug
 # from random import randint
 # dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(1, 6)
+# # dice_num = randint(1, 6)
+# dice_num = randint(0, 5)
 # print(dice_imgs[dice_num])
+# print(dice_imgs[0])
+# print(dice_imgs[1])
+# print(dice_imgs[2])
+# print(dice_imgs[3])
+# print(dice_imgs[4])
+# print(dice_imgs[5])
+# # print(dice_imgs[6])
 
 # # Play Computer
-# year = int(input("What's your year of birth?"))
-# if year > 1980 and year < 1994:
-#   print("You are a millenial.")
-# elif year > 1994:
-#   print("You are a Gen Z.")
+year = int(input("What's your year of birth?"))
+if year > 1980 and year < 1994:
+  print("You are a millenial.")
+elif year >= 1994:
+  print("You are a Gen Z.")
+else:
+    print(f"Your input: {year}")
 
 # # Fix the Errors
 # age = input("How old are you?")
