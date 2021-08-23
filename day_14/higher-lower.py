3a812cbbe9b636bc9f4d1be48e8d2c27f074a480
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2021-08-23 08:23:18 +0200

    Section 14: Day 14 - Beginner - Higher Lower Game Project
    132. Introduction & Program Requirements for the Higher Lower Game
    133. Solution & Walkthrough of the Higher Lower Game
    134. Study Tip: Set Reminders in Your Calendar to Review

---
+++ day_14/higher-lower.py
@@ -0,0 +1,41 @@
+from game_data import data
+from art import logo
+from art import vs
+from random import randint
+
+# print("\nWho has more instagram followers in millions:")
+compareA = randint(0,len(data))
+compareB = randint(0,len(data))
+game_on = True
+user_score = 0
+
+print(logo)
+
+while game_on:
+    print(f"Name A: {data[compareA]['name']},\n profesion: {data[compareA]['description']},\n country of birth: {data[compareA]['country']}")
+
+    print(vs)
+
+    print(f"Name B: {data[compareB]['name']},\n profesion: {data[compareB]['description']},\n country of birth: {data[compareB]['country']}")
+
+    user_answer = input("Who has more followers? Type 'A' or 'B': ")
+    correct_answer = ""
+    if data[compareA]['follower_count'] > data[compareB]['follower_count']:
+        correct_answer = "A"
+        compareB = randint(0,len(data))
+    else:
+        correct_answer = "B"
+        compareA = compareB
+        compareB = randint(0,len(data))
+
+    if user_answer == correct_answer:
+        print("You win")
+        user_score += 1
+        print(f"Curren score: {user_score}")
+        game_on = True
+
+    else:
+        print(F"Sorry, that's wrong. Final score: {user_score}")
+        game_on = False
+
+# print(data[])
