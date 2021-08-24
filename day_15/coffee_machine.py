9421f93047d8168c62c63d2d3009ae23f07bfff3
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2021-08-24 08:17:52 +0200

    Section 15: Day 15 - Intermediate - Local Development Environment Setup & the Coffee Machine 137. PyCharm's Charming Features (while you wait for the download to finish) 138. How to Install PyCharm on Windows 140. Introduction & Requirements for the Coffee Machine

---
+++ day_15/coffee_machine.py
@@ -0,0 +1,12 @@
+from coffee_machine_data import MENU
+from coffee_machine_data import resources
+
+print(resources)
+print(MENU)
+
+# TODO: 1. Print Report of coffee maschine
+# TODO: 1. Prompt user by asking “​What would you like? (espresso/latte/cappuccino):​”
+#           a. Check the user’s input to decide what to do next.
+#           b. The prompt should show every time action has completed, e.g. once the drink is dispensed. The prompt should show again to serve the next customer.
+
+print("Hmmm")
