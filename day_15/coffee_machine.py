34eb81a52d886957592a1aee28f8afe80bb3aa18
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2021-08-25 08:10:52 +0200

    Section 15: Day 15 - Intermediate - Local Development Environment Setup & the Coffee Machine 137. PyCharm's Charming Features (while you wait for the download to finish) 138. How to Install PyCharm on Windows 140. Introduction & Requirements for the Coffee Machine

--- day_15/coffee_machine.py
+++ day_15/coffee_machine.py
@@ -1,12 +1,50 @@
 from coffee_machine_data import MENU
 from coffee_machine_data import resources
 
-print(resources)
-print(MENU)
+# print(resources)
+# print(MENU)
+
+what_you_want = input("What would you like? (espresso/latte/cappuccino):")
+
+print("Please insert coins.")
+
+
+def check_for_correct_input_coins(coin):
+    while not coin.isnumeric():
+        coin = input("Only positive numbers:")
+    return int(coin)
+
+
+quarters = input("How many quarters?:")
+quarters = check_for_correct_input_coins(quarters)
+
+dimes = input("How many dimes?:")
+dimes = check_for_correct_input_coins(dimes)
+
+nickles = input("How many nickles?:")
+nickles = check_for_correct_input_coins(nickles)
+
+pennies = input("How many pennies?:")
+pennies = check_for_correct_input_coins(pennies)
+
+
+def calculate_amount(quarter, dime, nickle, pennie):
+    return (quarter * 25 + dime * 10 + nickle * 5 + pennie) / 100
+
+
+total_amount = calculate_amount(quarters, dimes, nickles, pennies)
+
+print(f"How much money in machine.{total_amount}")
+print(f"{what_you_want} is costing: {MENU[what_you_want]['cost']}")
+# print(f"{type(MENU[what_you_want]['cost'])}")
+# print(f"{type(total_amount)}")
+# print(f"{total_amount - MENU[what_you_want]['cost']}")
+
+print(f"Here is: {total_amount - MENU[what_you_want]['cost']:5.2f}$ in change.")
 
 # TODO: 1. Print Report of coffee maschine
 # TODO: 1. Prompt user by asking “​What would you like? (espresso/latte/cappuccino):​”
 #           a. Check the user’s input to decide what to do next.
 #           b. The prompt should show every time action has completed, e.g. once the drink is dispensed. The prompt should show again to serve the next customer.
 
-print("Hmmm")
+# print("Hmmm")
