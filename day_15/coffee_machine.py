079e6ab487500aff8431422c86068076cf255fd7
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2021-08-27 08:13:41 +0200

    Section 15: Day 15 - Intermediate - Local Development Environment Setup & the Coffee Machine 137. PyCharm's Charming Features (while you wait for the download to finish) 138. How to Install PyCharm on Windows 140. Introduction & Requirements for the Coffee Machine

--- day_15/coffee_machine.py
+++ day_15/coffee_machine.py
@@ -1,6 +1,5 @@
 from coffee_machine_data import MENU
 from coffee_machine_data import resources
-from coffee_machine_supply import check_resources
 
 # print(resources)
 # print(MENU)
@@ -14,6 +13,47 @@ def check_for_correct_input_coins(coin):
     while not coin.isnumeric():
         coin = input("Only positive numbers:")
     return int(coin)
+
+
+def check_resources(user_order):
+    sufficient_resources = 0
+    for key, x in MENU[user_order]['ingredients'].items():
+        print(f"Resources: {resources[key]} vs Order: {MENU[user_order]['ingredients'][key]}")
+        if MENU[user_order]['ingredients'][key] <= resources[key]:
+            # resources[key] = resources[key] - MENU[order]['ingredients'][key]
+            # print(f"{resources[key]}")
+            sufficient_resources += 1
+    if sufficient_resources == 3:
+        return "Thank you for your business."
+    elif 'y' == input("Do you want add resources to machine? (y/n)"):
+        add_resources(user_order)
+
+
+def add_resources(order):
+    if 'y' == input("Do you want to add difference? (y/n)"):
+        add_resources_difference(order)
+    else:
+        for key, x in resources.items():
+            resources[key] = resources[key] + int(input(f"How much you want to add? Correctly in machine {resources[key]}"))
+
+
+def add_resources_difference(order):
+    for key, x in MENU[order]['ingredients'].items():
+        if MENU[order]['ingredients'][key] > resources[key]:
+            resources[key] = MENU[order]['ingredients'][key] - resources[key]
+
+
+def remove_resources(order):
+    for key, x in MENU[order]['ingredients'].items():
+        if MENU[order]['ingredients'][key] <= resources[key]:
+            # print(f"Resources: {resources[key]} vs Order: {MENU[order]['ingredients'][key]}")
+            resources[key] = resources[key] - MENU[order]['ingredients'][key]
+            # print(f"{resources[key]}")
+
+
+def print_order(order):
+    for key, x in MENU[order]['ingredients'].items():
+        print(f"Resources: {resources[key]} vs Order: {MENU[order]['ingredients'][key]}")
 
 
 quarters = input("How many quarters?:")
@@ -38,8 +78,9 @@ total_amount = calculate_amount(quarters, dimes, nickles, pennies)
 if total_amount >= MENU[order]['cost']:
     print(f"How much money in machine.{total_amount}")
     print(f"{order.title()} is costing: {MENU[order]['cost']}")
-    print(f"Do we have all the ingredients. {check_resources(order)}")
+    print(f"Place order: {check_resources(order)}")
     print(f"Here is: {total_amount - MENU[order]['cost']:5.2f}$ in change.")
+    print(f"Your order:\n {print_order(order)}")
 else:
     print("You put not enough.")
 
