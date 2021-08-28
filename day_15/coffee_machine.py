3b771902b12df5a55b548aa30d155737b6ec0cc0
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2021-08-28 08:03:12 +0200

    Section 15: Day 15 - Intermediate - Local Development Environment Setup & the Coffee Machine 137. PyCharm's Charming Features (while you wait for the download to finish) 138. How to Install PyCharm on Windows 140. Introduction & Requirements for the Coffee Machine

--- day_15/coffee_machine.py
+++ day_15/coffee_machine.py
@@ -1,10 +1,11 @@
 from coffee_machine_data import MENU
 from coffee_machine_data import resources
 
-# print(resources)
+# print(current_resources_in_machine)
 # print(MENU)
 
 order = input("What would you like? (espresso/latte/cappuccino):")
+current_resources_in_machine = resources
 
 print("Please insert coins.")
 
@@ -15,45 +16,47 @@ def check_for_correct_input_coins(coin):
     return int(coin)
 
 
-def check_resources(user_order):
-    sufficient_resources = 0
+def check_current_resources_in_machine(user_order):
+    sufficient_current_resources_in_machine = 0
     for key, x in MENU[user_order]['ingredients'].items():
-        print(f"Resources: {resources[key]} vs Order: {MENU[user_order]['ingredients'][key]}")
-        if MENU[user_order]['ingredients'][key] <= resources[key]:
-            # resources[key] = resources[key] - MENU[order]['ingredients'][key]
-            # print(f"{resources[key]}")
-            sufficient_resources += 1
-    if sufficient_resources == 3:
+        print(f"Resources: {current_resources_in_machine[key]} "
+              f"vs Order: {MENU[user_order]['ingredients'][key]}")
+        if MENU[user_order]['ingredients'][key] <= current_resources_in_machine[key]:
+            # current_resources_in_machine[key] = current_resources_in_machine[key] - MENU[order]['ingredients'][key]
+            # print(f"{current_resources_in_machine[key]}")
+            sufficient_current_resources_in_machine += 1
+    if sufficient_current_resources_in_machine == 3:
         return "Thank you for your business."
-    elif 'y' == input("Do you want add resources to machine? (y/n)"):
-        add_resources(user_order)
+    elif 'y' == input("Do you want add current_resources_in_machine to machine? (y/n)"):
+        add_current_resources_in_machine(user_order)
 
 
-def add_resources(order):
+def add_current_resources_in_machine(user_order):
     if 'y' == input("Do you want to add difference? (y/n)"):
-        add_resources_difference(order)
+        add_current_resources_in_machine_difference(user_order)
     else:
-        for key, x in resources.items():
-            resources[key] = resources[key] + int(input(f"How much you want to add? Correctly in machine {resources[key]}"))
+        for key, x in current_resources_in_machine.items():
+            add_resource = int(input(f"How much you want to add? Correctly in machine {current_resources_in_machine[key]}"))
+            current_resources_in_machine[key] = current_resources_in_machine[key] + add_resource
 
 
-def add_resources_difference(order):
+def add_current_resources_in_machine_difference(user_order):
+    for key, x in MENU[user_order]['ingredients'].items():
+        if MENU[user_order]['ingredients'][key] > current_resources_in_machine[key]:
+            current_resources_in_machine[key] = MENU[user_order]['ingredients'][key] - current_resources_in_machine[key]
+
+
+def remove_current_resources_in_machine(order):
     for key, x in MENU[order]['ingredients'].items():
-        if MENU[order]['ingredients'][key] > resources[key]:
-            resources[key] = MENU[order]['ingredients'][key] - resources[key]
-
-
-def remove_resources(order):
-    for key, x in MENU[order]['ingredients'].items():
-        if MENU[order]['ingredients'][key] <= resources[key]:
-            # print(f"Resources: {resources[key]} vs Order: {MENU[order]['ingredients'][key]}")
-            resources[key] = resources[key] - MENU[order]['ingredients'][key]
-            # print(f"{resources[key]}")
+        if MENU[order]['ingredients'][key] <= current_resources_in_machine[key]:
+            # print(f"current_resources_in_machine: {current_resources_in_machine[key]} vs Order: {MENU[order]['ingredients'][key]}")
+            current_resources_in_machine[key] = current_resources_in_machine[key] - MENU[order]['ingredients'][key]
+            # print(f"{current_resources_in_machine[key]}")
 
 
 def print_order(order):
     for key, x in MENU[order]['ingredients'].items():
-        print(f"Resources: {resources[key]} vs Order: {MENU[order]['ingredients'][key]}")
+        print(f"current_resources_in_machine: {current_resources_in_machine[key]} vs Order: {MENU[order]['ingredients'][key]}")
 
 
 quarters = input("How many quarters?:")
@@ -78,15 +81,15 @@ total_amount = calculate_amount(quarters, dimes, nickles, pennies)
 if total_amount >= MENU[order]['cost']:
     print(f"How much money in machine.{total_amount}")
     print(f"{order.title()} is costing: {MENU[order]['cost']}")
-    print(f"Place order: {check_resources(order)}")
+    print(f"Place order: {check_current_resources_in_machine(order)}, {order}")
     print(f"Here is: {total_amount - MENU[order]['cost']:5.2f}$ in change.")
     print(f"Your order:\n {print_order(order)}")
 else:
     print("You put not enough.")
 
-# TODO: 1. Print Report of coffee maschine
-# TODO: 1. Prompt user by asking “​What would you like? (espresso/latte/cappuccino):​”
-#           a. Check the user’s input to decide what to do next.
-#           b. The prompt should show every time action has completed, e.g. once the drink is dispensed. The prompt should show again to serve the next customer.
+# TODO: 1. Print Report of coffee maschine TODO: 1. Prompt user by asking “​What would you like? (
+#  espresso/latte/cappuccino):​” a. Check the user’s input to decide what to do next. b. The prompt should show every
+#  time action has completed, e.g. once the drink is dispensed. The prompt should show again to serve the next
+#  customer.
 
 # print("Hmmm")
---
+++ day_15/temp.py
@@ -0,0 +1,29 @@
+from coffee_machine_data import resources, MENU
+
+
+# def check_resources(user_order):
+#     sufficient_resources = 0
+#     for key, x in MENU[user_order]['ingredients'].items():
+#         print(f"Resources: {resources[key]} vs Order: {MENU[user_order]['ingredients'][key]}")
+#         if MENU[user_order]['ingredients'][key] <= resources[key]:
+#             # resources[key] = resources[key] - MENU[order]['ingredients'][key]
+#             # print(f"{resources[key]}")
+#             sufficient_resources += 1
+#     if sufficient_resources == 3:
+#         return "Thank you for your business."
+#     # elif 'y' == input("Do you want add resources to machine? (y/n)"):
+#     #     add_resources(user_order)
+#     return "Done"
+#
+#
+# order = "cappuccino"
+#
+# print(f"Place order: {check_resources(order)}")
+
+current_resources_in_machine = resources
+
+# print(f"{current_resources_in_machine}")
+
+for key, x in current_resources_in_machine.items():
+    print(f"key: {key}, resources: {x}")
+
