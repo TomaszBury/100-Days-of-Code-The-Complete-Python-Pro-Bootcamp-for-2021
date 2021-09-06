8fa38ef684124291033e106c283ca24402b5b57d
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2021-09-06 11:32:57 +0200

    Section 17: Day 17 - Intermediate - The Quiz Project & the Benefits of OOP
    151. Day 17 Goals: what we will make by the end of the day
    152. How to create your own Class in Python Progress cannot be changed for this item
    153. Working with Attributes, Class Constructors and the __init__() Function
    154. Adding Methods to a Class
    155. Quiz Project Part 1: Creating the Question Class
    156. Quiz Project Part 2: Creating the List of Question Objects from the Data
    157. Quiz Project Part 3: The QuizBrain and the next_question() Method
    158. Quiz Project Part 4: How to continue showing new Questions
    159. Quiz Project Part 5: Checking Answers and Keeping Score
    160. The Benefits of OOP: Use Open Trivia DB to Get New Questions

--- day_17/quiz-game-start/main.py
+++ day_17/quiz-game-start/main.py
@@ -1,14 +1,21 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
 for x in question_data:
     # print(f"{x['text']}:{x['answer']}")
-    question = Question(x['text'], x['answer'])
+    question = Question(x['question'], x['correct_answer'])
     question_bank.append(question)
 
 # for x in question_bank:
 #     print(x.text)
 
-quiz_brain = []
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You've completed the quiz.")
+print(f"Your final score was: {quiz.score} / {quiz.question_number}")
