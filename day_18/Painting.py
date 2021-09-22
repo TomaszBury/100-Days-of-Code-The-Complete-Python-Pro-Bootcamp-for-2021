995ff50c8630f523b3426a64a4470454cee68f84
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2021-09-22 08:31:50 +0200

    Section 18: Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI) 162. Day 18 Goals: what we will make by the end of the day 163. Understanding Turtle Graphics and How to use the Documentation 164. Turtle Challenge 1 - Draw a Square 165. Importing Modules, Installing Packages, and Working with Aliases 166. Turtle Challenge 2 - Draw a Dashed Line 167. Turtle Challenge 3 - Drawing Different Shapes 168. Turtle Challenge 4 - Generate a Random Walk 169. Python Tuples and How to Generate Random RGB Colours 170. Turtle Challenge 5 - Draw a Spirograph 171. The Hirst Painting Project Part 1 - How to Extract RGB Values from Images

--- day_18/Painting.py
+++ day_18/Painting.py
@@ -1,6 +1,65 @@
+import random
 from turtle import Turtle, Screen
+import colorgram
 
 tom = Turtle()
-
 screen = Screen()
+screen.colormode(255)
+
+# Extract 6 colors from an image.
+colors = colorgram.extract('danmien_hirst_complete_sport.jpg', 15)
+
+# colorgram.extract returns Color objects, which let you access
+# RGB, HSL, and what proportion of the image was that color.
+# first_color = colors[0]
+# rgb = first_color.rgb  # e.g. (255, 151, 210)
+
+tom.pensize(15)
+tom.penup()
+tom.left(180)
+tom.forward(400)
+tom.right(270)
+tom.forward(400)
+tom.left(90)
+tom.pendown()
+
+number_of_lines = 40
+length_of_color = 1
+length_of_blank = 20
+
+
+# tom.forward(100)
+# tom.right(180)
+# tom.forward(250)
+# tom.left(90)
+# tom.forward(300)
+# tom.left(90)
+
+def move_to_new_line():
+    tom.penup()
+    tom.left(90)
+    tom.forward(2 * 10)
+    tom.left(90)
+    tom.forward(number_of_lines * (length_of_color + length_of_blank))
+    tom.right(180)
+    tom.pendown()
+
+
+def random_color():
+    return random.choice(colors).rgb
+
+
+def paint_new_line():
+    for _ in range(0, number_of_lines):
+        tom.color(random_color())
+        tom.forward(length_of_color)
+        tom.penup()
+        tom.forward(length_of_blank)
+        tom.pendown()
+
+
+for _ in range(0, number_of_lines):
+    paint_new_line()
+    move_to_new_line()
+
 screen.exitonclick()
