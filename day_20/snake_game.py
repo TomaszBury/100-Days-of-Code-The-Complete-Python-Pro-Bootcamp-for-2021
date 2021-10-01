8a7b72fb704fcaaff8a109baa2cade97d22a6785
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2021-10-01 08:36:36 +0200

    Section 21: Day 21 - Intermediate - Build the Snake Game Part 2: Inheritance & List Slicing 187. Day 21 Goals: what we will make by the end of the day 188. Class Inheritance, 189. Detect Collisions with Food 190. Create a Scoreboard and Keep Score

--- day_20/snake_game.py
+++ day_20/snake_game.py
@@ -1,5 +1,7 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,13 +11,14 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
-
 
 # for segment_number in range(0, 3):
 #     new_segment = Turtle()
@@ -27,13 +30,17 @@ screen.onkey(snake.right, "Right")
 #     snake.append(new_segment)
 
 
-
 game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    # Detect collision with food.
+    if snake.head_of_the_snake.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
 
 
 screen.exitonclick()
