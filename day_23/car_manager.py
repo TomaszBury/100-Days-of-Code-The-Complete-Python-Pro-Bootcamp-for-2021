ef93814035f49d7710ff98994f4448d6c358b8e7
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2021-10-26 19:19:52 +0200

    205. Day 23 Goals: what you will make by the end of the day 206. Choose Your Difficulty 207. How to use the Starter Code 208. Step 1 - Check out how the game play works 209. Step 2 - Break down the Problem 210. Solution to Step 3 - Create the Player Behaviour 211. Solution to Step 4 - Create the Car Behaviour 212. Solution to Step 5 - Detect when the Turtle collides with a Car *squish* 213. Solution to Step 6 - Detect when the Player has reached the other side 214. Solution to Step 7 - Add the Scoreboard and Game Over sequence 215. This course is not about typing out code

--- day_23/car_manager.py
+++ day_23/car_manager.py
@@ -1,7 +1,57 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager():
+    def __init__(self):
+        self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+    def create_car(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-258, 250)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+            if car.xcor() < -280:
+                car.hideturtle()
+                self.all_cars.remove(car)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
+
+
+        # super().__init__()
+        # self.color(random.choice(COLORS))
+        # self.penup()
+        # self.shape("square")
+        # self.shapesize(stretch_wid=.5, stretch_len=2)
+        # self.goto(position)
+
+    # def go_left(self):
+    #     if self.xcor() < 280:
+    #         new_x = self.xcor() + MOVE_INCREMENT
+    #         self.goto(new_x, self.ycor())
+    #         return True
+    #     else:
+    #         return False
+    #
+    # def go_right(self):
+    #     if self.xcor() > -280:
+    #         new_x = self.xcor() - MOVE_INCREMENT
+    #         self.goto(new_x, self.ycor())
+    #         return False
+    #     else:
+    #         return True
--- day_23/player.py
+++ day_23/player.py
@@ -1,7 +1,38 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.shape("turtle")
+        self.penup()
+        self.step = 20
+        self.go_to_start()
+        self.left(90)
+
+    def go_up(self):
+        if self.ycor() < 300:
+            self.forward(MOVE_DISTANCE)
+        # if self.ycor() < 280:
+        #     new_y = self.ycor() + 20
+        #     self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        if self.ycor() > - 300:
+            self.backward(MOVE_DISTANCE)
+            # new_y = self.ycor() - 20
+            # self.goto(self.xcor(), new_y)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
