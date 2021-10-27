dad8720cd6bd9161b04cd67632d84d0fa8c29171
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2021-10-27 07:35:26 +0200

    216. Day 24 Goals: what you will make by the end of the day 217. Add a High Score to the Snake Game 218. How to Open, Read, and Write to Files using the "with" Keyword 219. Challenge: Read and Write the High Score to a File in Snake 220. Understand Relative and Absolute File Paths

---
+++ day_24/main.py
@@ -0,0 +1,21 @@
+# file = open('my_file.txt')
+# file.close()
+
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# with open("new_file.txt", mode='w') as file:
+#     file.write("\nNwe text.")
+#
+# with open("new_file.txt") as file:
+#     print(file.read())
+#
+# with open("new_file.txt", mode='a') as file:
+#     file.write("\nNwe text.")
+#
+# with open("new_file.txt") as file:
+#     print(file.read())
+
+with open("../../../Desktop/my_file.txt") as file:
+    print(file.read())
