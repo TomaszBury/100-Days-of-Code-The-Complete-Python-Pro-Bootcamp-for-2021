c631f1cb46286c198d085eb76c5812d336b11bf9
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2021-11-14 13:51:56 +0100

    287. How to Send Emails with Python using SMTP 288. Working with the datetime Module 289. Challenge 1 - Send Motivational Quotes on Mondays via Email 290. Automated Birthday Wisher Project Challenge

--- day_32/birthday-wisher-extrahard-start/main.py
+++ day_32/birthday-wisher-extrahard-start/main.py
@@ -1,24 +1,30 @@
 import smtplib
 import datetime as dt
 import random
+import pandas as pd
 
 MY_EMAIL = "erica.schultz54@ethereal.email"
 PASSWORD = "AMUEYVnjQHdmDEFUu3"
 WHEN_TO_SEND = "11/14/21"
+
 ##################### Extra Hard Starting Project ######################
+
+
+birthdays = pd.read_csv("birthdays.csv")
+print(birthdays.values)
+
+print(f"{birthdays[birthdays.name == 'Test'].month[0]}/{birthdays[birthdays.name == 'Test'].day[0]}"
+      f"/{birthdays[birthdays.name == 'Test'].year[0]-1900}")
 now = dt.datetime.now()
 print(now.strftime("%x"))
-if now.strftime("%x") == WHEN_TO_SEND:
-    print("great")
+# if now.strftime("%x") == WHEN_TO_SEND:
+#     print("great")
 
 # 1. Update the birthdays.csv
 
 # 2. Check if today matches a birthday in the birthdays.csv
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME]
+# with the person's actual name from birthdays.csv
 
 # 4. Send the letter generated in step 3 to that person's email address.
-
-
-
-
