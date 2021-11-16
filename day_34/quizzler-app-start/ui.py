981e65dfa2e990b390139cf0925698d28e688513
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2021-11-16 18:31:05 +0100

    303. Solution & Walkthrough for getting Trivia Questions 305. Class based Tkinter UI 306. Python Typing & Showing the Next Question in the GUI 307. Python Typing: Type Hints and Arrows -> 309. Give Feedback to the Player, Keep Score and Fix the Bugs =)

--- day_34/quizzler-app-start/ui.py
+++ day_34/quizzler-app-start/ui.py
@@ -1,21 +1,66 @@
+import time
 from tkinter import *
+from quiz_brain import QuizBrain
+
 THEME_COLOR = "#375362"
+
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+        # Label -- SCORE
         self.score_label = Label(text="Score: 0", bg=THEME_COLOR, fg='white',
-                                 padx=20, pady=20, font=('Arial', 12, 'italic'))
+                                 padx=20, font=('Arial', 12, 'italic'))
         self.score_label.grid(column=1, row=0, sticky=E)
+        # Canvas question
         self.canvas = Canvas(width=300, height=250)
-
         self.question_text = self.canvas.create_text(150, 125,
-                                                     text="test test test test test test test test test test test test test test test test test test test test test ",
+                                                     text="",
                                                      width=300,
-                                                     font=('Arial', 20, 'italic'))
+                                                     font=('Arial', 20, 'italic'),
+                                                     fill=THEME_COLOR)
         self.canvas.config(bg="white", highlightthickness=0)
-        self.canvas.grid(row=1, column=0, columnspan=2)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        self.get_next_question()
+
+        # Buttons
+        false_img = PhotoImage(file="images/false.png")
+        true_img = PhotoImage(file="images/true.png")
+
+        self.false_button = Button(image=false_img, highlightthickness=0, command=self.false_button)
+        self.true_button = Button(image=true_img, highlightthickness=0, command=self.true_button)
+
+        self.false_button.grid(row=2, column=1, padx=20)
+        self.true_button.grid(row=2, column=0, padx=20)
+
+        # Loop
         self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text='This is the end.')
+            self.false_button.config(state="disabled")
+            self.true_button.config(state="disabled")
+
+    def false_button(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+
+    def true_button(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def give_feedback(self, is_right: bool):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg='red')
+        self.window.after(1000, self.get_next_question)
