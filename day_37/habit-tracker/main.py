37426f0746ea80631a1d723668d2c206c2ccbccb
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2021-11-30 07:29:09 +0100

    322. Day 37 Goals: what you will make by the end of the day 323. HTTP Post Request 324. Advanced Authentication using an HTTP Header 325. Challenge: Add a Pixel to the Habit Tracker using a Post 326. Autofilling today's date using strftime 327. How to use HTTP Put and Delete Requests

--- day_37/habit-tracker/main.py
+++ day_37/habit-tracker/main.py
@@ -1,15 +1,57 @@
+from datetime import datetime
+
 import requests
+
+USERNAME = "thoma"
+TOKEN = "eNLH6EzZrKaJ6o2T2tkw"
+GRAPH_ID = "graph1"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
+
 user_params = {
-    "token": "eNLH6EzZrKaJ6o2T2tkw",
-    "username": "thoma",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 
 }
-# "message":"Success. Let's visit https://pixe.la/@thoma , it is your profile page!","isSuccess":true
-# response = requests.post(url=pixela_endpoint, json=user_params)
-# print(response.text)
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime(year=2021, month=11, day=29)
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "420.69",
+}
+
+# response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data = {
+    "quantity": "4.5"
+}
+
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+response = requests.delete(url=delete_endpoint, headers=headers)
+
+print(response.text)
---
+++ day_37/habit-tracker/setup_user.py
@@ -0,0 +1,12 @@
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": "eNLH6EzZrKaJ6o2T2tkw",
+    "username": "thoma",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+
+}
+# "message":"Success. Let's visit https://pixe.la/@thoma , it is your profile page!","isSuccess":true
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
