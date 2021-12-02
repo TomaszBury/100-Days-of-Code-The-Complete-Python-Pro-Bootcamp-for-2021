0e9f3b7b93e548f4e14133393cb02f556d38bcaf
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2021-12-02 07:28:15 +0100

    335. Day 39 Goals: what you will make by the end of the day 336. Step 1 - Choose Your Path and Download the Starting Project

---
+++ day_39/flight-deals-start/data_manager.py
@@ -0,0 +1,4 @@
+class DataManager:
+    #This class is responsible for talking to the Google Sheet.
+    sheet_endpoint = "https://api.sheety.co/6ed30aa04f21710d833b8b543a03f91c/flightDeals/prices"
+    pass
