bdd7fdfb5328a809ea575f4d5130528a13196aef
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2021-12-28 20:11:21 +0100

    fun

--- day_51/main.py
+++ day_51/main.py
@@ -1,15 +1,32 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.common.by import By
+#
+# chrome_driver_path = "./chromedriver_win32/chromedriver.exe"
+# s = Service(chrome_driver_path)
+# driver = webdriver.Chrome(service=s)
+#
+# driver.set_window_size(1920, 1200)
+# # print(driver.get_window_size())
+#
+# driver.get("https://www.python.org")
+#
+# driver.quit()
+#
 
-chrome_driver_path = "./chromedriver_win32/chromedriver.exe"
-s = Service(chrome_driver_path)
-driver = webdriver.Chrome(service=s)
+text = "Python goes b" + "r" * 10
+print(text)
 
-driver.set_window_size(1920, 1200)
-# print(driver.get_window_size())
+# for i in range(10):
+#     print(i)
+#
 
-driver.get("https://www.python.org")
+for i in range(9):
+    print(i)
 
-driver.quit()
+print("0123456789")
+
+text = "0123456789"
+for i in text:
+    print(i)
 
